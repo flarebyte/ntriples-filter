e94f1a5c21be64440553752a29d87e47eece709d
olih <olih@users.noreply.github.com>
2017-07-29 15:30:56 +0100

    wip

--- src/Ntriples/Filter.elm
+++ src/Ntriples/Filter.elm
@@ -8,6 +8,7 @@ module Ntriples.Filter exposing (Triple, TripleFilter, filterTriples)
 -}
 import List exposing (..)
 import Maybe
+import String
 
 -- TYPES
 
@@ -16,6 +17,58 @@ type alias Triple = { subject : String, predicate : String, object: String }
 
 {-| A rdf triple filter -}
 type alias TripleFilter = { subject : Maybe String, predicate : Maybe String, object: Maybe String }
+
+type FieldId = Subject | Predicate | Obj
+
+type FieldComparator = Ignore
+  | IsEmpty
+  | Equal String
+  | StartsWith String
+  | Contains String
+
+type FilterExpr
+    = Boolean Bool
+    | Not FilterExpr
+    | And FilterExpr FilterExpr
+    | Or FilterExpr FilterExpr
+    | Field FieldComparator FieldId
+
+{-| get the string of a triple field -}
+stringOfField: FieldId -> Triple -> String
+stringOfField fieldId triple =
+  case fieldId of
+    Subject -> triple.subject
+    Predicate -> triple.predicate
+    Obj -> triple.object
+
+{-| compare a single field and return true if must be selected -}
+fieldCompare: FieldComparator -> String -> Bool
+fieldCompare comparator value =
+  case comparator of
+    Ignore ->
+      True
+    IsEmpty ->
+        String.isEmpty value
+    Equal ref ->
+        value == ref
+    StartsWith ref ->
+        String.startsWith ref value
+    Contains ref ->
+        String.contains ref value
+
+tripleCompare: FilterExpr -> Triple -> Bool
+tripleCompare expr triple =
+  case expr of
+    Boolean value ->
+      value
+    Not expr ->
+        not (tripleCompare expr triple)
+    And a b ->
+        (tripleCompare a triple) && (tripleCompare b triple)
+    Or a b  ->
+        (tripleCompare a triple) || (tripleCompare b triple)
+    Field comp fieldId ->
+      fieldCompare comp (stringOfField fieldId triple)
 
 {-| filter a list of triples -}
 filterTriples: TripleFilter -> List Triple -> List Triple
