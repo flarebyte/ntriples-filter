ef81f8c949d5f5daf599fa916b88f8f462ff865c
olih <olih@users.noreply.github.com>
2017-07-30 17:18:44 +0100

    wip

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,15 +1,12 @@
 module Tests exposing (..)
 
-import Test exposing (..)
-import List exposing (..)
+import Test exposing (describe, test, Test)
 import Expect
-import Fuzz exposing (list, int, tuple3)
 import Ntriples.Filter exposing (..)
-import Maybe exposing(..)
 
 t1 = { subject = "s1", predicate = "name", object = "subject1" }
-t2 = { subject = "s2", predicate = "name", object = "subject2" }
-t3 = { subject = "s3", predicate = "name", object = "subject3" }
+t2 = { subject = "s2", predicate = "name", object = "" }
+t3 = { subject = "s3", predicate = "name", object = "some subject3" }
 t4 = { subject = "s4", predicate = "name", object = "subject4" }
 t5 = { subject = "s5", predicate = "name", object = "subject5" }
 t5a = { subject = "s5", predicate = "desc", object = "desc5" }
@@ -24,13 +21,13 @@ all =
             [ test "filter by object value" <|
                 \() ->
                     Expect.equal
-                    (filterTriples (WithObject (Equals "subject3")) allTriples)
-                    [t3]
+                    (filterTriples (WithObject (Equals "subject4")) allTriples)
+                    [t4]
               , test "filter by object value and predicate" <|
                 \() ->
                     Expect.equal
-                    (filterTriples (And (WithPredicate (Equals "name"))(WithObject (Equals "subject3")) ) allTriples)
-                    [t3]
+                    (filterTriples (And (WithPredicate (Equals "name"))(WithObject (Equals "subject4")) ) allTriples)
+                    [t4]
               , test "filter by Subject" <|
                 \() ->
                     Expect.equal
@@ -41,5 +38,10 @@ all =
                     Expect.equal
                     (filterTriples (Not( WithSubject (Equals "s5"))) allTriples)
                     [t1, t2, t3, t4]
+              , test "filter by empty" <|
+                \() ->
+                    Expect.equal
+                    (filterTriples (WithObject (IsEmpty)) allTriples)
+                    [t2]
             ]
         ]
